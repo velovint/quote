3384ae9340a2580993102907f5bef3d195af8bfe
Vitali Yakavenka <vitali.yakavenka@equest.com>
2019-02-15 00:22:45 -0800

    Stream quotes

--- stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteHandler.java
+++ stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteHandler.java
@@ -1,23 +1,37 @@
 package io.spring.workshop.stockquotes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 import static org.springframework.web.reactive.function.BodyInserters.fromObject;
 import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
 
 @Component
 public class QuoteHandler {
-//    public Mono<ServerResponse> hello(ServerRequest request) {
-//        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromObject("Hello world"));
-//    }
+    private final Flux<Quote> quoteStream;
+
+    @Autowired
+    public QuoteHandler(QuoteGenerator quoteGenerator) {
+        quoteStream = quoteGenerator.fetchQuoteStream(Duration.ofMillis(1000)).share();
+    }
+
+    public Mono<ServerResponse> hello(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromObject("Hello world"));
+    }
 
     public Mono<ServerResponse> echo(ServerRequest request) {
         return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
                 .body(request.bodyToMono(String.class), String.class);
+    }
+
+    public Mono<ServerResponse> streamQuotes(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_STREAM_JSON).body(quoteStream, Quote.class);
     }
 }
--- stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteRouter.java
+++ stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteRouter.java
@@ -5,16 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.*;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
 @Configuration
 public class QuoteRouter {
     @Bean
     RouterFunction<ServerResponse> router(QuoteHandler quoteHandler) {
         return RouterFunctions
-                .route(RequestPredicates.GET("/hello"), quoteHandler::echo)
+                .route(GET("/hello"), quoteHandler::echo)
                 .andRoute(
-                        RequestPredicates.POST("/echo")
-                                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN))
-                                .and(RequestPredicates.contentType(MediaType.TEXT_PLAIN)),
-                        quoteHandler::echo);
+                        POST("/echo")
+                                .and(accept(MediaType.TEXT_PLAIN))
+                                .and(contentType(MediaType.TEXT_PLAIN)),
+                        quoteHandler::echo)
+                .andRoute(GET("/quotes"), quoteHandler::streamQuotes);
     }
 }
