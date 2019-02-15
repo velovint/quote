f12ec43ce272386ee7b79a0d1332277169907368
Vitali Yakavenka <vitali.yakavenka@equest.com>
2019-02-14 22:07:03 -0800

    Init'

---
+++ src/test/java/io/spring/workshop/quote/QuoteApplicationTests.java
@@ -0,0 +1,17 @@
+package io.spring.workshop.quote;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuoteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
