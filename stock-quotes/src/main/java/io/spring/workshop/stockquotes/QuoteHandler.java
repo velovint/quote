ba080dafd120b809c567fa0d895e2756fbd38981
Vitali Yakavenka <vitali.yakavenka@equest.com>
2019-02-14 23:33:40 -0800

    Add echo handler

--- stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteHandler.java
+++ stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteHandler.java
@@ -2,15 +2,22 @@ package io.spring.workshop.stockquotes;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.BodyInserters.fromObject;
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
 
 @Component
 public class QuoteHandler {
-    public Mono<ServerResponse> hello(ServerRequest request) {
-        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromObject("Hello world"));
+//    public Mono<ServerResponse> hello(ServerRequest request) {
+//        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN).body(fromObject("Hello world"));
+//    }
+
+    public Mono<ServerResponse> echo(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+                .body(request.bodyToMono(String.class), String.class);
     }
 }
--- stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteRouter.java
+++ stock-quotes/src/main/java/io/spring/workshop/stockquotes/QuoteRouter.java
@@ -2,6 +2,7 @@ package io.spring.workshop.stockquotes;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.*;
 
 @Configuration
@@ -9,6 +10,11 @@ public class QuoteRouter {
     @Bean
     RouterFunction<ServerResponse> router(QuoteHandler quoteHandler) {
         return RouterFunctions
-                .route(RequestPredicates.GET("/hello"), quoteHandler::hello);
+                .route(RequestPredicates.GET("/hello"), quoteHandler::echo)
+                .andRoute(
+                        RequestPredicates.POST("/echo")
+                                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN))
+                                .and(RequestPredicates.contentType(MediaType.TEXT_PLAIN)),
+                        quoteHandler::echo);
     }
 }
