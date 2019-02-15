90940e2d866b8cc70a5ea05e7f56e477c29a8411
Vitali Yakavenka <vitali.yakavenka@equest.com>
2019-02-14 22:18:23 -0800

    Add stock-quotes scaffolding

---
+++ stock-quotes/src/main/java/io/spring/workshop/stockquotes/StockQuoteApplication.java
@@ -0,0 +1,14 @@
+package io.spring.workshop.stockquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockQuoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockQuoteApplication.class, args);
+	}
+
+}
+
